01823321a9dabc1e502a445d8be0be0d7345870a
KIPROTYCH <kiprotych@gmail.com>
2023-09-23 00:01:21 +0300

    Completed Assignment.

--- server/models.py
+++ server/models.py
@@ -1,32 +1,61 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import validates
+from sqlalchemy.exc import IntegrityError
+
 db = SQLAlchemy()
 
 class Author(db.Model):
     __tablename__ = 'authors'
-    # Add validations and constraints 
 
     id = db.Column(db.Integer, primary_key=True)
-    name= db.Column(db.String, unique=True, nullable=False)
-    phone_number = db.Column(db.String)
-    created_at = db.Column(db.DateTime, server_default=db.func.now())
-    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+    name = db.Column(db.String(255), unique=True, nullable=False)
+    phone_number = db.Column(db.String(10), nullable=False)
 
-    def __repr__(self):
-        return f'Author(id={self.id}, name={self.name})'
+    @validates('name')
+    def validate_name(self, key, value):
+        if not value:
+            raise ValueError("Name must not be empty.")
+        return value
+
+    @validates('phone_number')
+    def validate_phone_number(self, key, value):
+        if len(value) != 10 or not value.isdigit():
+            raise ValueError("Phone number must be exactly ten digits.")
+        return value
 
 class Post(db.Model):
     __tablename__ = 'posts'
-    # Add validations and constraints 
 
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String, nullable=False)
-    content = db.Column(db.String)
-    category = db.Column(db.String)
-    summary = db.Column(db.String)
-    created_at = db.Column(db.DateTime, server_default=db.func.now())
-    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+    title = db.Column(db.String(255), nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    category = db.Column(db.String(100))
+    summary = db.Column(db.String(250))
 
+    @validates('title')
+    def validate_title(self, key, value):
+        if not value:
+            raise ValueError("Title is required.")
+        clickbait_keywords = ['clickbait', 'sensational', 'shocking', 'exclusive']
+        if any(keyword in value.lower() for keyword in clickbait_keywords):
+            raise ValueError("Title contains clickbait content.")
+        return value
 
-    def __repr__(self):
-        return f'Post(id={self.id}, title={self.title} content={self.content}, summary={self.summary})'
+    @validates('content')
+    def validate_content_length(self, key, value):
+        if len(value) <= 250:
+            raise ValueError("Content must be at least 250 characters long.")
+        return value
+
+    @validates('summary')
+    def validate_summary_length(self, key, value):
+        if len(value) >= 250:
+            raise ValueError("Summary cannot be more than 250 characters long.")
+        return value
+
+    @validates('category')
+    def validate_category(self, key, value):
+        allowed_categories = ['Fiction', 'Non-Fiction', 'Science Fiction', 'Mystery']
+        if value not in allowed_categories:
+            raise ValueError("Invalid category.")
+        return value
